c23af0204b14915ecbc1b477f1f500de4e519b09
Youth <yakdpa@gmail.com>
2026-03-06 12:15:27 +0800

    更新 1142110895_cn.js

--- Library/Caches/com.nan.cac/1142110895_cn.js
+++ Library/Caches/com.nan.cac/1142110895_cn.js
@@ -3,7 +3,7 @@
   "id": 1142110895,
   "bundle": "com.ss.iphone.ugc.Aweme",
   "name": "抖音",
-  "version": "37.9.0",
+  "version": "37.8.0",
   "version_id": 882889676,
   "version_release_timestamp": "2026-03-05T07:39:20Z",
   "price": null,
