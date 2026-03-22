a0afcc74cd55f6b7524e504b48a12b764fc3bc25
Youth <yakdpa@gmail.com>
2026-03-22 20:10:30 +0800

    Update WeChat version from 8.0.70 to 8.0.60

--- Library/Caches/com.nan.cac/414478124_cn.js
+++ Library/Caches/com.nan.cac/414478124_cn.js
@@ -3,7 +3,7 @@
   "id": 414478124,
   "bundle": "com.tencent.xin",
   "name": "微信",
-  "version": "8.0.70",
+  "version": "8.0.60",
   "version_id": 883464393,
   "version_release_timestamp": "2026-03-21T14:14:33Z",
   "price": null,
